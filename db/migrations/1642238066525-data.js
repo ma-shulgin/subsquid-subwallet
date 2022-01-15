866d963577457c0b541d06de6c5cbcc2c8200239
Alexander Belopashentsev <abelopashentsev@gmail.com>
2022-01-15 14:16:56 +0500

    trying to deploy

--- db/migrations/1642238066525-data.js
+++ db/migrations/1642238066525-data.js
@@ -1,5 +1,5 @@
-module.exports = class data1642190178399 {
-  name = 'data1642190178399'
+module.exports = class data1642238066525 {
+  name = 'data1642238066525'
 
   async up(db) {
     await db.query(`CREATE TABLE "balance_transaction" ("id" character varying NOT NULL, "date" TIMESTAMP WITH TIME ZONE NOT NULL, "block_hash" text NOT NULL, "block_number" integer NOT NULL, "extrinisic_hash" text, "data" jsonb, "event" character varying(18), CONSTRAINT "PK_5e3fd7a79cf10c4cd192e5648dc" PRIMARY KEY ("id"))`)
